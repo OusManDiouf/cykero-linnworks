c13190e5397a3c257fc6d713abf7498dec1a7f5e
oussoulessou <java-89@hotmail.fr>
2025-10-05 16:43:16 +0000

    Clean up PM2 configuration in `ecosystem.config.cjs`
    - Fixed typo in `cwd` property.
    - Removed outdated comments for environment and logging.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,20 +2,16 @@
 module.exports = {
   apps: [
     {
-      cw: '/var/www/html/linnworks.cykero.eu',
       name: 'linnworks-app',
+      cwd: '/var/www/html/linnworks.cykero.eu',
       script: 'dist/src/main.js',
-      instances: 1, // keep 1 due to scheduled jobs
+      instances: 1,
       exec_mode: 'fork',
       watch: false,
       autorestart: true,
       max_memory_restart: '512M',
       node_args: ['--enable-source-maps'],
-
-      // Load your existing .env file; adjust path if needed
       env_file: '.env',
-
-      // Logging (optional but recommended)
       out_file: './logs/app.out.log',
       error_file: './logs/app.err.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
